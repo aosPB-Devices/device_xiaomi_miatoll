488f357f0a215c3571879adc4b7bc9d3cc12ebd1
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-03-28 17:43:39 +0600

    miatoll: bringup for aosPB

--- aospb_miatoll.mk
+++ aospb_miatoll.mk
@@ -5,28 +5,17 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
-# Inherit some common Pixelage stuff.
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+# Inherit some common aosPB stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
+$(call inherit-product, device/xiaomi/miatoll/flags.mk)
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
-# Pixelage
-PIXELAGE_BUILDTYPE := OFFICIAL
-PIXELAGE_MAINTAINER := clarencelol
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-
-PRODUCT_NAME := pixelage_miatoll
+PRODUCT_NAME := aospb_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
